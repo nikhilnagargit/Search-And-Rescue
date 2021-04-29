43a33ba2af405132e5cb5110aac274250ed5ee5c
laxit <laxityadav123@gmail.com>
2021-04-29 13:23:08 +0530

    get route of missing aircraft created

--- routes/api/reportAircraft.js
+++ routes/api/reportAircraft.js
@@ -1,47 +1,67 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const Airplane = require('../../models/Airplane');
 const router = express.Router();
 
 //@route GET api/reportAircraft
 //@desc   report the missing aircraft
 //@access Public
-router.get('/', (req, res) => {
-  res.send('Testing the report aircraft route');
+router.get('/', async (req, res) => {
+  const plane = await Airplane.find({});
+  res.status(201).json(plane);
 });
 
 //@route POST api/reportAircraft
 //@desc report the missing aircraft
 //@access Private
-router.post('/', async (req, res) => {
-  const {
-    latitude,
-    longitude,
-    velocity,
-    direction,
-    weather,
-    description,
-    altitude,
-    title,
-    category,
-  } = req.body;
-  if (latitude) {
-    const plane = new Airplane({
+router.post(
+  '/',
+  // [
+  //   check('latitude', 'Please enter latitude').not().isEmpty(),
+  //   check('longitude', 'Please enter longitude').not().isEmpty(),
+  //   check('velocity', 'Please enter velocity').isNumeric().not().isEmpty(),
+  //   check('direction', 'Please enter direction').not().isEmpty(),
+  //   check('weather', 'Please enter weather').not().isEmpty(),
+  //   check('description', 'Please enter description').not().isEmpty(),
+  //   check('altitude', 'Please enter altitude').not().isEmpty(),
+  //   check('title', 'Please enter title').not().isEmpty(),
+  //   check('category', 'Please enter category').not().isEmpty(),
+  // ],
+  async (req, res) => {
+    //const errors = validationResult(req);
+    console.log(req.body);
+    // if (!errors.isEmpty()) {
+    //   return res.status(400).json({ errors: errors.array() });
+    // }
+    const {
       latitude,
       longitude,
-      description,
       velocity,
       direction,
       weather,
+      description,
       altitude,
       title,
       category,
-    });
-    await plane.save();
-    console.log('One airplane reported in db');
-    res.status(201).json(plane);
-  } else {
-    res.status(400).json({ err: 'at least input latitude field.' });
-  }
-});
+    } = req.body;
+    if (latitude) {
+      const plane = new Airplane({
+        latitude,
+        longitude,
+        description,
+        velocity,
+        direction,
+        weather,
+        altitude,
+        title,
+        category,
+      });
+      await plane.save();
+      console.log('One airplane reported in db');
+      res.status(201).json(plane);
+    } else {
+      res.status(400).json({ err: 'at least input latitude field.' });
+    }
+  });
 
 module.exports = router;
