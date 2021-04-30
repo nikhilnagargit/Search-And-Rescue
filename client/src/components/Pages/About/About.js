7dd4cd12a15d4265537f9d2ec6231c934fd2566f
laxit <laxityadav123@gmail.com>
2021-04-30 17:23:08 +0530

    about page created

--- client/src/components/Pages/About/About.js
+++ client/src/components/Pages/About/About.js
@@ -1,8 +1,27 @@
 import './About.scss';
 import React from 'react';
+import amico from '../../../images/amico.png';
+import jecrc from '../../../images/jecrc.png';
+import user from '../../../images/user.png';
 
 const About = () => {
-  return <div>I am about page</div>;
+  return (
+    <div className="about">
+      <h1>Search And Rescue operations for missing Aircraft</h1>
+      <div className="aboutinside">
+        <div className="team">
+          <ul>
+            <li><img src={user} /><span>Nikhil Nagar</span></li>
+            <li><img src={user} /><span>Laxit Yadav</span></li>
+            <li><img src={user} /><span>Lakhan Kachhawa</span></li>
+            <li><img src={user} /><span>Karamveer Singh Rathore</span></li>
+          </ul>
+          <img className="jecrc" src={jecrc} alt=""></img>
+        </div>
+        <img className="amico" src={amico} alt=""></img>
+      </div>
+    </div>
+  );
 };
 
 export default About;
