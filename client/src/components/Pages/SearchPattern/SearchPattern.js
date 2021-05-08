6cfc33ce44c493601f22cebfa209e27559f6b4b8
nikhilnagargit <nikhilnagarcps@gmail.com>
2021-05-08 22:00:35 +0530

    added pattern page, and multiple layers in the map

--- client/src/components/Pages/SearchPattern/SearchPattern.js
+++ client/src/components/Pages/SearchPattern/SearchPattern.js
@@ -1,8 +1,25 @@
 import './SearchPattern.scss';
 import React from 'react';
+import SearchMap from '../../layout/SearchMap/SearchMap';
+import { connect } from 'react-redux';
+import { getSearchArea } from '../../../actions/area';
 
-const SearchPattern = () => {
-  return <div>I am search pattern page</div>;
+const mapStateToProps = (state) => {
+  return {
+    areaData: state.searchAreaReducer,
+  };
 };
 
-export default SearchPattern;
+const SearchPattern = (props) => {
+  return (
+    <div className='search-pattern-main'>
+      <div className='top'></div>
+      <div className='middle'>
+        <SearchMap areaData={props.areaData} />
+      </div>
+      <div className='side'></div>
+    </div>
+  );
+};
+
+export default connect(mapStateToProps, { getSearchArea })(SearchPattern);
--- client/src/components/layout/SearchMap/SearchMap.js
+++ client/src/components/layout/SearchMap/SearchMap.js
@@ -1,7 +1,14 @@
 import './SearchMap.scss';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import airplane_pin from '../../../images/airplane_pin.png';
-import { GeoJSON } from 'react-leaflet';
+import {
+  GeoJSON,
+  Circle,
+  LayersControl,
+  LayerGroup,
+  FeatureGroup,
+  Rectangle,
+} from 'react-leaflet';
 
 import hospital_pin from '../../../images/hospital_pin.png';
 import station_pin from '../../../images/station_pin.png';
@@ -18,6 +25,7 @@ const mapStateToProps = (state) => {
 const SearchMap = (props) => {
   // fetch data for search area
 
+  // render the corresponding image from assets
   const getImageFromPoint = (type) => {
     if (type === 'hospital') {
       return hospital_pin;
@@ -29,7 +37,8 @@ const SearchMap = (props) => {
   const apitoken =
     'pk.eyJ1IjoibmlraGlsbmFnYXIxMjMiLCJhIjoiY2tvM3l1MGRhMWU5czJ4b2JteWd6NHdoayJ9.ME2Il0_kSq5tr19J6m2UHQ';
 
-  const tileurl = `https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/{z}/{x}/{y}?access_token=${apitoken}`;
+  const tileurl_option1 = `https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/{z}/{x}/{y}?access_token=${apitoken}`;
+  const tileurl_option2 = `https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/{z}/{x}/{y}?access_token=${apitoken}`;
   return (
     <MapContainer
       className='mysearchmap'
@@ -41,67 +50,91 @@ const SearchMap = (props) => {
       }
       zoom={12}
     >
-      <TileLayer url={tileurl} />
+      <LayersControl position='topright'>
+        {/* set the base layer option 1 */}
+        <LayersControl.BaseLayer checked name='  Streets View'>
+          <TileLayer url={tileurl_option1} />
+        </LayersControl.BaseLayer>
 
-      {Object.entries(props.areaData).length !== 0 && (
-        <>
+        {/* set the base layer option 2*/}
+        <LayersControl.BaseLayer name=' Satellite View'>
+          <TileLayer url={tileurl_option2} />
+        </LayersControl.BaseLayer>
+
+        {/* layer, which shows search area */}
+
+        <LayersControl.Overlay name='Search Area Layer' checked>
           <GeoJSON key={1} data={props.areaData.geojson} />
+        </LayersControl.Overlay>
 
-          {props.areaData.help_points.features.map((item, index) => {
-            return (
+        {/* aircraft and its popup layer */}
+        <LayersControl.Overlay name='Aircraft LKP' checked>
+          <LayerGroup>
+            {Object.entries(props.aircraft).length !== 0 && (
               <Marker
-                key={index}
-                icon={L.icon({
-                  iconUrl: getImageFromPoint(item.properties.type),
-                  iconSize: 30,
-                })}
-                position={[
-                  item.geometry.coordinates[1],
-                  item.geometry.coordinates[0],
-                ]}
+                icon={L.icon({ iconUrl: airplane_pin, iconSize: 40 })}
+                position={[props.aircraft.latitude, props.aircraft.longitude]}
               >
                 <Popup>
-                  <b>{item.properties.type}</b>
+                  <h3>Last known Information of Aircraft</h3>
+                  Title: <b>{props.aircraft.title}</b>
                   <br />
-
-                  {item.properties.title}
+                  Location:
+                  <b>
+                    {props.aircraft.latitude},{props.aircraft.longitude}
+                  </b>
+                  <br />
+                  Category : {props.aircraft.category}
+                  <br />
+                  Direction :{props.aircraft.direction}
+                  <br />
+                  Velocity :{props.aircraft.velocity}
+                  <br />
+                  Altitude :{props.aircraft.altitude}
+                  <br />
+                  Direction :{props.aircraft.direction}
+                  <br />
+                  Weather :{props.aircraft.weather}
+                  <br />
+                  Description :{props.aircraft.description}
                 </Popup>
               </Marker>
-            );
-          })}
-        </>
-      )}
+            )}
+          </LayerGroup>
+        </LayersControl.Overlay>
 
-      {Object.entries(props.aircraft).length !== 0 && (
-        <Marker
-          icon={L.icon({ iconUrl: airplane_pin, iconSize: 40 })}
-          position={[props.aircraft.latitude, props.aircraft.longitude]}
-        >
-          <Popup>
-            <h3>Last known Information of Aircraft</h3>
-            Title: <b>{props.aircraft.title}</b>
-            <br />
-            Location:
-            <b>
-              {props.aircraft.latitude},{props.aircraft.longitude}
-            </b>
-            <br />
-            Category : {props.aircraft.category}
-            <br />
-            Direction :{props.aircraft.direction}
-            <br />
-            Velocity :{props.aircraft.velocity}
-            <br />
-            Altitude :{props.aircraft.altitude}
-            <br />
-            Direction :{props.aircraft.direction}
-            <br />
-            Weather :{props.aircraft.weather}
-            <br />
-            Description :{props.aircraft.description}
-          </Popup>
-        </Marker>
-      )}
+        {/* layers, which shows additionl points like hospitals, bases etc */}
+        <LayersControl.Overlay checked name='Additional Spots Layer'>
+          <LayerGroup>
+            {Object.entries(props.areaData).length !== 0 && (
+              <>
+                {props.areaData.help_points.features.map((item, index) => {
+                  return (
+                    <Marker
+                      key={index}
+                      icon={L.icon({
+                        iconUrl: getImageFromPoint(item.properties.type),
+                        iconSize: 30,
+                      })}
+                      position={[
+                        item.geometry.coordinates[1],
+                        item.geometry.coordinates[0],
+                      ]}
+                    >
+                      <Popup>
+                        <b>{item.properties.type}</b>
+                        <br />
+
+                        {item.properties.title}
+                      </Popup>
+                    </Marker>
+                  );
+                })}
+              </>
+            )}
+          </LayerGroup>
+        </LayersControl.Overlay>
+      </LayersControl>
     </MapContainer>
   );
 };
