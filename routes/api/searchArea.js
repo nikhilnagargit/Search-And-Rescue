e7d2cb0fd0d0cfc6761fba189fd48a26cb6bbf3e
laxit <laxityadav123@gmail.com>
2021-05-16 22:32:44 +0530

    route updated

--- routes/api/searchArea.js
+++ routes/api/searchArea.js
@@ -7,13 +7,14 @@ const router = express.Router();
 //@desc get the search area
 //@access Public
 router.get('/:id', async (req, res) => {
+  let side = req.query.side;
   let plane = await Airplane.findById(req.params.id);
   let { latitude, longitude, direction, velocity, altitude } = plane;
   let distance = calcDistance(altitude, velocity);
   let newLatLon = calcLatLon(latitude, longitude, direction, distance);
 
   //make a geojson for new latitude and longitude
-  let sqJson = calcSquareJson(newLatLon, 15, direction);
+  let sqJson = calcSquareJson(newLatLon, side, direction);
   res.status(201).json(sqJson);
 });
 
