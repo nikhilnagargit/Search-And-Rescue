1bf3e5e15d5bd36edd99d3a2f21089f89f85852f
nikhilnagargit <nikhilnagarcps@gmail.com>
2021-06-06 11:56:58 +0530

    asdf

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
+const path = require('path');
 
 app.use(express.json());
 
 //connect the database
 connectDB();
-
-app.get('/', (req, res) => res.send('API running.'));
 
 //Define the routes
 app.use('/api/reportAircraft', require('./routes/api/reportAircraft'));
@@ -17,9 +16,13 @@ app.use('/api/rescueTeam', require('./routes/api/rescueTeam'));
 
 const PORT = process.env.PORT || 5000;
 
-// just for production
+// serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
+  // set static folder
   app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
